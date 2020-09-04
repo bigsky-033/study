9c1435abf3d05863c9403f67cc0340a182505ceb
bigsky-park <ptm302@gmail.com>
2020-09-04 23:14:19 +0900

    leetcode: Add solutions for p01299

---
+++ problem-solving/leetcode/p_01299.go
@@ -0,0 +1,18 @@
+package main
+
+import "math"
+
+// 1299. Replace Elements with Greatest Element on Right Side, https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+
+func replaceElements(arr []int) []int {
+	max := math.MinInt32
+	for i := len(arr) - 1; i >= 0; i-- {
+		cur := arr[i]
+		arr[i] = max
+		if cur > max {
+			max = cur
+		}
+	}
+	arr[len(arr)-1] = -1
+	return arr
+}
