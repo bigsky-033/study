19afe33694f7d57029bae98825f778fa561a54c8
bigsky-park <ptm302@gmail.com>
2020-09-05 21:14:21 +0900

    leetcode: Add solutions for p00448, p00950

---
+++ problem-solving/leetcode/p_00950.go
@@ -0,0 +1,17 @@
+package main
+
+// 905. Sort Array By Parity, https://leetcode.com/problems/sort-array-by-parity/
+
+func sortArrayByParity(A []int) []int {
+	evenIdx := 0
+
+	for i := 0; i < len(A); i++ {
+		if A[i]%2 == 0 {
+			if A[evenIdx]%2 != 0 {
+				A[i], A[evenIdx] = A[evenIdx], A[i]
+			}
+			evenIdx++
+		}
+	}
+	return A
+}
