e25271a57565509daa6dc34287d3d9ef27f2ad93
bigsky-park <ptm302@gmail.com>
2020-09-10 20:55:52 +0900

    leetcode: Add solutions for p00033

---
+++ problem-solving/leetcode/p_00033.go
@@ -0,0 +1,43 @@
+package main
+
+func search(nums []int, target int) int {
+	if len(nums) < 1 {
+		return -1
+	}
+
+	// Find index of the smallest element
+	lo, hi := 0, len(nums)-1
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		if nums[mid] > nums[hi] {
+			lo = mid + 1
+		} else {
+			hi = mid
+		}
+	}
+
+	// Find out which side target resides. Left or right.
+	minIdx := lo
+	lo, hi = 0, len(nums)-1
+	if target >= nums[minIdx] && target <= nums[hi] {
+		lo = minIdx
+	} else {
+		hi = minIdx
+	}
+
+	// Do regular binary search
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		if target > nums[mid] {
+			lo = mid + 1
+		} else {
+			hi = mid
+		}
+	}
+
+	if nums[lo] == target {
+		return lo
+	} else {
+		return -1
+	}
+}
