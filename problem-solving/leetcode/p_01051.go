3cc4e07de86177cbdda20073c4688065e7183857
bigsky-park <ptm302@gmail.com>
2020-09-14 22:20:58 +0900

    leetcode: Add solutions for p01051

---
+++ problem-solving/leetcode/p_01051.go
@@ -0,0 +1,19 @@
+package main
+
+import "sort"
+
+// 1051. Height Checker, https://leetcode.com/problems/height-checker/
+
+func heightChecker(heights []int) int {
+	copied := make([]int, len(heights))
+	copy(copied, heights)
+	sort.Ints(copied)
+
+	res := 0
+	for i := range heights {
+		if heights[i] != copied[i] {
+			res++
+		}
+	}
+	return res
+}
