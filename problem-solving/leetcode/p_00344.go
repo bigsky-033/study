7bc130d6229f38aa72971858caab339f097d2685
bigsky-park <ptm302@gmail.com>
2020-09-19 22:35:34 +0900

    Add solutions for p00344

---
+++ problem-solving/leetcode/p_00344.go
@@ -0,0 +1,18 @@
+package main
+
+// 344. Reverse String, https://leetcode.com/problems/reverse-string/
+
+// Recursive version
+func reverseString(s []byte) {
+	doReverseString(s, 0, len(s)-1)
+}
+
+func doReverseString(s []byte, left int, right int) {
+	if left >= right {
+		return
+	}
+	s[left], s[right] = s[right], s[left]
+	left++
+	right--
+	doReverseString(s, left, right)
+}
