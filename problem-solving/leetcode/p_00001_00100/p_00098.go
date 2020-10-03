43bb617b1add72ea4df0d72273e66a6737b2a942
bigsky-park <ptm302@gmail.com>
2020-10-03 22:35:04 +0900

    leetcode: Add solution for p00098

---
+++ problem-solving/leetcode/p_00001_00100/p_00098.go
@@ -0,0 +1,41 @@
+package p_00001_00100
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isValidBST(root *TreeNode) bool {
+	isValid := true
+	isFirst := true
+	prevValue := 0
+	inorderTraversal(root, &isValid, &isFirst, &prevValue)
+	return isValid
+}
+
+func inorderTraversal(root *TreeNode, isValid *bool, isFirst *bool, prevValue *int) {
+	if !(*isValid) || root == nil {
+		return
+	}
+	inorderTraversal(root.Left, isValid, isFirst, prevValue)
+	if *isFirst {
+		*prevValue = root.Val
+		*isFirst = false
+	} else {
+		if root.Val <= *prevValue {
+			*isValid = false
+		}
+		*prevValue = root.Val
+	}
+	inorderTraversal(root.Right, isValid, isFirst, prevValue)
+}
