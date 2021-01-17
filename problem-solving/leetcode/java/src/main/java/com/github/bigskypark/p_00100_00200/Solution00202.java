e29a5ba62d9fc3f95ee8c13403273f0a2ccdb9ff
bigsky-park <ptm302@gmail.com>
2021-01-17 11:09:24 +0900

    leetcode: Add solution for p00202

---
+++ problem-solving/leetcode/java/src/main/java/com/github/bigskypark/p_00100_00200/Solution00202.java
@@ -0,0 +1,27 @@
+package com.github.bigskypark.p_00100_00200;
+
+// 202. Happy Number, https://leetcode.com/problems/happy-number/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution00202 {
+  public boolean isHappy(int n) {
+    char[] chars = String.valueOf(n).toCharArray();
+    Set<Integer> visited = new HashSet<>();
+    for (; ; ) {
+      int sum = 0;
+      for (char c : chars) {
+        sum += (int) Math.pow(c - '0', 2);
+      }
+      if (visited.contains(sum)) {
+        return false;
+      }
+      visited.add(sum);
+      if (sum == 1) {
+        return true;
+      }
+      chars = String.valueOf(sum).toCharArray();
+    }
+  }
+}
