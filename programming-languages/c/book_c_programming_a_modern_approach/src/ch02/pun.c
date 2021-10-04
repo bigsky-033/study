754f901bc2bb42e0cb4d475e329582a416a199b8
bigsky.park.033@gmail.com <bigsky.park.033@gmail.com>
2021-10-04 17:54:01 +0900

    c: Add notes and sample code for knk book

---
+++ programming-languages/c/book_c_programming_a_modern_approach/src/ch02/pun.c
@@ -0,0 +1,12 @@
+/*
+ * Name: pun.c
+ * Purpose: Prints a bad pun.
+ * Author: K. N. King
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  printf("To C, or not to C: that is the qustion.\n");
+  return 0;
+}
