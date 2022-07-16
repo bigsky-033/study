fa601840692f4a0704609f8d6a38e0ea16eaab58
bigsky-033 <bigsky.park.033@gmail.com>
2022-07-16 18:23:42 +0900

    leetcode: Add p1480 solution with golang

---
+++ problem-solving/leetcode/golang/p1400/p1480_running_sum_of_1d_array/sol_test.go
@@ -0,0 +1,49 @@
+package p1480runningsumof1darray
+
+import "testing"
+
+func Test_runningSum(t *testing.T) {
+	tests := []struct {
+		name string
+		input []int
+		output []int
+	} {
+		{
+			name: "example 1",
+			input: []int{1,2,3,4},
+			output: []int{1,3,6,10},
+		},
+		{
+			name: "example 2",
+			input: []int{1,1,1,1,1},
+			output: []int{1,2,3,4,5},
+		},
+		{
+			name: "example 3",
+			input: []int{3,1,2,10,1},
+			output: []int{3,4,6,16,17},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func (t2 *testing.T)  {
+			actual := runningSum(test.input)
+
+			if len(actual) != len(test.output) {
+				t2.Fatalf("actual length: %d expected length: %d", len(actual), len(test.output))
+			}
+			for i := 0; i < len(actual); i++ {
+				if actual[i] != test.output[i] {
+					t2.Fatalf("value is not matched. actual[%d]: %d, expected[%d]: %d\n", i, actual[i], i, test.output[i])
+				}
+			}
+		})
+	}
+}
+
+func runningSum(nums []int) []int {
+	for i := 1; i < len(nums); i++ {
+		nums[i] = nums[i] + nums[i-1]
+	}
+	return nums
+}
