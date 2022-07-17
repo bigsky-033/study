e17412a42e81af2d58ef92a24fffb4885b6e383c
bigsky-033 <bigsky.park.033@gmail.com>
2022-07-17 09:53:51 +0900

    leetcode: Apply gofmt

--- problem-solving/leetcode/golang/p1400/p1480_running_sum_of_1d_array/sol_test.go
+++ problem-solving/leetcode/golang/p1400/p1480_running_sum_of_1d_array/sol_test.go
@@ -4,29 +4,29 @@ import "testing"
 
 func Test_runningSum(t *testing.T) {
 	tests := []struct {
-		name string
-		input []int
+		name   string
+		input  []int
 		output []int
-	} {
+	}{
 		{
-			name: "example 1",
-			input: []int{1,2,3,4},
-			output: []int{1,3,6,10},
+			name:   "example 1",
+			input:  []int{1, 2, 3, 4},
+			output: []int{1, 3, 6, 10},
 		},
 		{
-			name: "example 2",
-			input: []int{1,1,1,1,1},
-			output: []int{1,2,3,4,5},
+			name:   "example 2",
+			input:  []int{1, 1, 1, 1, 1},
+			output: []int{1, 2, 3, 4, 5},
 		},
 		{
-			name: "example 3",
-			input: []int{3,1,2,10,1},
-			output: []int{3,4,6,16,17},
+			name:   "example 3",
+			input:  []int{3, 1, 2, 10, 1},
+			output: []int{3, 4, 6, 16, 17},
 		},
 	}
 
 	for _, test := range tests {
-		t.Run(test.name, func (t *testing.T)  {
+		t.Run(test.name, func(t *testing.T) {
 			actual := runningSum(test.input)
 
 			if len(actual) != len(test.output) {
--- problem-solving/leetcode/golang/p1600/p1672_richest_customer_wealth/sol_test.go
+++ problem-solving/leetcode/golang/p1600/p1672_richest_customer_wealth/sol_test.go
@@ -4,23 +4,23 @@ import "testing"
 
 func Test_maximumWealth(t *testing.T) {
 	tests := []struct {
-		name string
-		input [][]int
+		name   string
+		input  [][]int
 		output int
-	} {
+	}{
 		{
-			name: "example 1",
-			input: [][]int{{1,2,3},{3,2,1}},
+			name:   "example 1",
+			input:  [][]int{{1, 2, 3}, {3, 2, 1}},
 			output: 6,
 		},
 		{
-			name: "example 2",
-			input: [][]int{{1,5},{7,3},{3,5}},
+			name:   "example 2",
+			input:  [][]int{{1, 5}, {7, 3}, {3, 5}},
 			output: 10,
 		},
 		{
-			name: "example 3",
-			input: [][]int{{2,8,7},{7,1,3},{1,9,5}},
+			name:   "example 3",
+			input:  [][]int{{2, 8, 7}, {7, 1, 3}, {1, 9, 5}},
 			output: 17,
 		},
 	}
@@ -30,25 +30,25 @@ func Test_maximumWealth(t *testing.T) {
 			actual := maximumWealth(test.input)
 
 			if actual != test.output {
-				t.Errorf("value is not matched. expected: %d actual: %d", 
-				test.output, actual)
+				t.Errorf("value is not matched. expected: %d actual: %d",
+					test.output, actual)
 			}
 		})
 	}
 }
 
 func maximumWealth(accounts [][]int) int {
-		mw := -1
+	mw := -1
 
-		for i := 0; i < len(accounts); i++ {
-			sum := 0
-			for j := 0; j < len(accounts[i]); j++ {
-				sum += accounts[i][j]
-			}
-			mw = max(mw, sum)
+	for i := 0; i < len(accounts); i++ {
+		sum := 0
+		for j := 0; j < len(accounts[i]); j++ {
+			sum += accounts[i][j]
 		}
+		mw = max(mw, sum)
+	}
 
-    return mw
+	return mw
 }
 
 func max(a, b int) int {
